ceb3eaf1548536b687576233fbfd068c4094426e
lucas mateus <lucas.mts1812@gmail.com>
2025-06-23 22:31:45 -0300

    Added test for CNPJ and added corrections for CNPJ class

--- src/main/java/valueobjects/CNPJ.java
+++ src/main/java/valueobjects/CNPJ.java
@@ -11,20 +11,12 @@ public class CNPJ {
     }
 
     public void setCNPJ(String stringCNPJ){
-        if (stringCNPJ.length() == 18) {
-            stringCNPJ = stringCNPJ.replace(".", "")
+        if (validateCNPJ(stringCNPJ)) {
+            this.CNPJraw = stringCNPJ.replace(".", "")
                 .replace("-", "")
                 .replace("/", "");
-        }
-
-        if (validateCNPJ(stringCNPJ)) {
-            this.CNPJraw = stringCNPJ;
-            this.CNPJ = String.format(
-                "%s.%s.%s/%s-%s",
-                stringCNPJ.substring(0, 2), stringCNPJ.substring(2, 5),
-                stringCNPJ.substring(5, 8), stringCNPJ.substring(8, 12), stringCNPJ.substring(12, 14)
-            );
-        } else throw new Error("Invalid CNPJ format.");   
+            this.CNPJ = stringCNPJ;
+        } else throw new IllegalArgumentException("Invalid CNPJ format.");   
     }
 
     public String getCNPJ(){
@@ -36,13 +28,21 @@ public class CNPJ {
     }
 
     private boolean validateCNPJ(String stringCNPJ) {
+        if (stringCNPJ == null) throw new NullPointerException("CNPJ must not be null!");
+
+        if (stringCNPJ.length() == 18) {
+            stringCNPJ = stringCNPJ.replace(".", "")
+                .replace("-", "")
+                .replace("/", "");
+        }
+
         if (
-            CNPJ.equals("00000000000000") || CNPJ.equals("11111111111111") ||
-            CNPJ.equals("22222222222222") || CNPJ.equals("33333333333333") ||
-            CNPJ.equals("44444444444444") || CNPJ.equals("55555555555555") ||
-            CNPJ.equals("66666666666666") || CNPJ.equals("77777777777777") ||
-            CNPJ.equals("88888888888888") || CNPJ.equals("99999999999999") ||
-            (CNPJ.length() != 14)
+            stringCNPJ.equals("00000000000000") || stringCNPJ.equals("11111111111111") ||
+            stringCNPJ.equals("22222222222222") || stringCNPJ.equals("33333333333333") ||
+            stringCNPJ.equals("44444444444444") || stringCNPJ.equals("55555555555555") ||
+            stringCNPJ.equals("66666666666666") || stringCNPJ.equals("77777777777777") ||
+            stringCNPJ.equals("88888888888888") || stringCNPJ.equals("99999999999999") ||
+            (stringCNPJ.length() != 14)
         ) return false;
 
         char dig13, dig14;
@@ -52,7 +52,7 @@ public class CNPJ {
             sm = 0;
             weight = 2;
             for (i=11; i>=0; i--) {
-                num = (int)(CNPJ.charAt(i) - 48);
+                num = (int)(stringCNPJ.charAt(i) - 48);
                 sm = sm + (num * weight);
                 weight = weight + 1;
                 if (weight == 10) weight = 2;
@@ -65,7 +65,7 @@ public class CNPJ {
             sm = 0;
             weight = 2;
             for (i=12; i>=0; i--) {
-                num = (int)(CNPJ.charAt(i)- 48);
+                num = (int)(stringCNPJ.charAt(i)- 48);
                 sm = sm + (num * weight);
                 weight = weight + 1;
                 if (weight == 10) weight = 2;
@@ -76,10 +76,15 @@ public class CNPJ {
             else dig14 = (char)((11-remainder) + 48);
 
 
-            if ((dig13 == CNPJ.charAt(12)) && (dig14 == CNPJ.charAt(13))) return true;
+            if ((dig13 == stringCNPJ.charAt(12)) && (dig14 == stringCNPJ.charAt(13))) return true;
             else return false;
         } catch (InputMismatchException erro) {
             return false;
         }
+    }
+
+    @Override
+    public String toString() {
+        return getCNPJ();
     }
 }
