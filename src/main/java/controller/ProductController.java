631d41c6e8c8b5a8c9ffae50d0157fffcd2a2c4b
lucas mateus <lucas.mts1812@gmail.com>
2025-06-26 18:48:31 -0300

    Added ProductDAO to use database methods

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -4,18 +4,22 @@ import java.util.NoSuchElementException;
 
 import model.Product;
 import model.list.ProductList;
+import database.DAO.ProductDAO;;;
 
 public class ProductController {
     private final ProductList pList;
+    private final ProductDAO pDao; 
 
-    public ProductController(ProductList pList) {
+    public ProductController(ProductList pList, ProductDAO pDao) {
         this.pList = pList;
+        this.pDao = pDao;
     }
 
     public Product registerProduct(String pSku, String pName, String pDescription, int sId){
         try {
             Product product = new Product(pSku, pName, pDescription, sId);
-    
+            
+            pDao.insert(product);
             pList.addProduct(product);
 
             return product;
@@ -45,6 +49,8 @@ public class ProductController {
             product.setDescription(pDescription);
             product.setSupplierId(sId);
             
+            pDao.update(pId, product);
+
             return product;
         } catch (Exception e) {
             System.out.println("No product/supplier found with inserted id");
@@ -55,6 +61,7 @@ public class ProductController {
 
     public void removeProduct(int pId){
         try {
+            pDao.delete(pId);
             pList.removeProductById(pId);
         } catch (NoSuchElementException e) {
             System.out.println("No product found with inserted id");
