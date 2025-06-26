111602b2b7090b76a570985f45c9e0f7b451bcf1
lucas mateus <lucas.mts1812@gmail.com>
2025-06-25 22:23:22 -0300

    Fixed reference to model.Product

--- src/main/java/database/DAO/ProductDAO.java
+++ src/main/java/database/DAO/ProductDAO.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import models.Product;
+import model.Product;
 
 public class ProductDAO {
     private final Connection conn;
