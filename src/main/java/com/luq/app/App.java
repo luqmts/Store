03a41a967eaa60eca444da725c6e6c474240a24b
lucas mateus <lucas.mts1812@gmail.com>
2025-06-26 12:04:05 -0300

    Created SupplierView

--- src/main/java/com/luq/app/App.java
+++ src/main/java/com/luq/app/App.java
@@ -1,10 +1,26 @@
 package com.luq.app;
 
-/**
- * Hello world!
- */
+
+import controller.ProductController;
+import model.Supplier;
+import model.list.ProductList;
+import model.list.SupplierList;
+import valueobjects.CNPJ;
+import valueobjects.Mail;
+import valueobjects.Phone;
+import view.ProductView;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ProductList pList = new ProductList();
+        SupplierList sList = new SupplierList();
+        Supplier s1 = new Supplier(
+            1, "Sony Brasil LTDA.", new CNPJ("43.447.044/0004-10"), new Mail("sony@mail.com"), new Phone("11000001111")
+        );
+        sList.addSupplier(s1);
+        
+        ProductController pController = new ProductController(pList, sList);
+        ProductView pView = new ProductView(pController);
+        pView.showProductMenu();
     }
 }
