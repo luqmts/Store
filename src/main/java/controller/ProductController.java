67fce377eb8affb5e980f7bad6effa2fd99cbc7a
lucas mateus <lucas.mts1812@gmail.com>
2025-06-29 15:57:36 -0300

    Controllers now use Service classes to CRUD operations

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,76 +1,28 @@
 package controller;
 
-import java.util.NoSuchElementException;
-
 import model.Product;
-import model.Supplier;
-import model.list.ProductList;
-
-import database.DAO.ProductDAO;
-import database.DAO.SupplierDAO;
+import service.ProductService;
 
 public class ProductController {
-    private final SupplierDAO sDao;
-    private final ProductDAO pDao; 
+    private final ProductService pService;
 
-    public ProductController(SupplierDAO sDao, ProductDAO pDao) {
-        this.sDao = sDao;
-        this.pDao = pDao;
+    public ProductController(ProductService pService) {
+        this.pService = pService;
     }
 
     public Product registerProduct(String pSku, String pName, String pDescription, int sId){
-        try {
-            Supplier supplier = sDao.getById(sId);
-            if (supplier == null) {
-                throw new NoSuchElementException("No supplier found with the given id.");
-            }
-            Product product = new Product(pSku, pName, pDescription, sId);
-            
-            pDao.insert(product);
-
-            return product;
-        } catch (NoSuchElementException e) {
-            System.out.println("No supplier found with inserted id.");
-        }
-
-        return null;
+        return pService.registerProduct(pSku, pName, pDescription, sId);
     }
 
     public void showAllProducts(){
-        ProductList pList = pDao.get(sDao);
-        try {
-            for (Product product : pList.get()) {
-                System.out.println(product.toString());
-            }
-        } catch (NoSuchElementException e) {
-            System.out.println("No products registered");
-        }
+       pService.showAllProducts();
     }
 
     public Product updateProduct(int pId, String pSku, String pName, String pDescription, int sId){
-        try {
-            Product product = pDao.getById(pId);
-    
-            product.setSku(pSku);
-            product.setName(pName);
-            product.setDescription(pDescription);
-            product.setSupplierId(sId);
-            
-            pDao.update(pId, product);
-
-            return product;
-        } catch (Exception e) {
-            System.out.println("No product/supplier found with inserted id");
-        }
-
-        return null;
+        return pService.updateProduct(pId, pSku, pName, pDescription, sId);
     }
 
     public void removeProduct(int pId){
-        try {
-            pDao.delete(pId);
-        } catch (NoSuchElementException e) {
-            System.out.println("No product found with inserted id");
-        }
+        pService.deleteProduct(pId);
     }
 }
--- src/main/java/controller/SupplierController.java
+++ src/main/java/controller/SupplierController.java
@@ -1,79 +1,29 @@
 package controller;
 
-import model.list.SupplierList;
-
-import java.util.NoSuchElementException;
+import service.SupplierService;
 
 import model.Supplier;
-import valueobjects.CNPJ;
-import valueobjects.Mail;
-import valueobjects.Phone;
-
-import database.DAO.SupplierDAO;
 
 public class SupplierController {
-    private final SupplierDAO sDao;
-
-    public SupplierController(SupplierDAO sDao){
-        this.sDao = sDao;
+    protected final SupplierService sService;
+    
+    public SupplierController(SupplierService sService){
+        this.sService = sService;
     }
 
     public Supplier registerSupplier(String sName, String sCNPJ, String sMail, String sPhone){
-        try {
-            CNPJ cnpj = new CNPJ(sCNPJ);
-            Mail mail = new Mail(sMail);
-            Phone phone = new Phone(sPhone);
-    
-            Supplier supplier = new Supplier(sName, cnpj, mail, phone);
-
-            sDao.insert(supplier);    
-            return supplier;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        return null;
+        return sService.registerSupplier(sName, sCNPJ, sMail, sPhone);
     }
 
     public void showAllSuppliers(){
-        SupplierList sList = sDao.get();
-
-        try {
-            for (Supplier supplier : sList.get()) {
-                System.out.println(supplier.toString());
-            }
-        } catch (NoSuchElementException e) {
-            System.out.println("No suppliers registered");
-        }
+        sService.showAllSuppliers();
     }
 
     public Supplier updateSupplier(int sId, String sName, String sCNPJ, String sMail, String sPhone){
-        try {
-            Supplier supplier = sDao.getById(sId);
-            CNPJ CNPJ = new CNPJ(sCNPJ);
-            Mail mail = new Mail(sMail);
-            Phone phone = new Phone(sPhone);
-    
-            supplier.setName(sName);
-            supplier.setCNPJ(CNPJ);
-            supplier.setMail(mail);
-            supplier.setPhone(phone);
-
-            sDao.update(sId, supplier);
-    
-            return supplier;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-
-        return null;
+        return sService.updateSupplier(sId, sName, sCNPJ, sMail, sPhone);
     }
 
     public void removeSupplier(int sId){
-        try {
-            sDao.delete(sId);
-        } catch (NoSuchElementException e) {
-            System.out.println(e.getMessage());
-        }
+        sService.deleteSupplier(sId);
     }
 }
