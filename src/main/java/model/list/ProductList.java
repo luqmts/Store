bbc62e476b3ae1456a571fa150ff01134bb2fc2e
lucas mateus <lucas.mts1812@gmail.com>
2025-06-29 16:38:17 -0300

    Classes that inherit from List superclass now don't create more itself lists, using from superclass

--- src/main/java/model/list/ProductList.java
+++ src/main/java/model/list/ProductList.java
@@ -8,30 +8,25 @@ import model.Supplier;
 
 
 public class ProductList extends List<Product> {
-    private ArrayList<Product> pList;
-    
-    public ProductList(){
-        pList = new ArrayList<Product>();
-    }
-
     public Product getBySku(String productSku) {
-        for (Product product : pList) {
+        for (Product product : items) {
             if (productSku == product.getSku()) return product;
         }
 
-        throw new NoSuchElementException();
+        throw new NoSuchElementException("No Product found with SKU: " + productSku);
     }
 
     public ArrayList<Product> getBySupplier(Supplier supplier) {
         ArrayList<Product> pListBySupplier = new ArrayList<Product>();
 
-        for (Product product : pList) {
+        for (Product product : items) {
             if (supplier.getId() == product.getSupplierId())
                 pListBySupplier.add(product);
         }
 
         if (pListBySupplier.isEmpty()) 
-            throw new NoSuchElementException();
-        else return pListBySupplier;
+            throw new NoSuchElementException("No Product found with Supplier: " + supplier.getName()); 
+        
+        return pListBySupplier;
     }
 }
--- src/main/java/model/list/SupplierList.java
+++ src/main/java/model/list/SupplierList.java
@@ -1,56 +1,15 @@
 package model.list;
 
-import java.util.ArrayList;
 import java.util.NoSuchElementException;
 
 import model.Supplier;
 
 public class SupplierList extends List<Supplier> {
-    private ArrayList<Supplier> sList;
-    
-    public SupplierList(){
-        sList = new ArrayList<Supplier>();
-    }
-
-    public void add(Supplier supplier) {
-        sList.add(supplier);
-    }
-
-    public void removeByIndex(int sIndex) {
-        try {
-            sList.remove(sIndex);
-        } catch (IndexOutOfBoundsException e ) {
-            System.out.println(String.format("Supplier with index '%d' not found.", sIndex));
-        }
-    }
-
-    public void removeById(int sId) {
-        try {
-            Supplier supplier = getById(sId);
-            sList.remove(supplier);
-        } catch (NoSuchElementException e){
-            System.out.println(String.format("Supplier with id '%d' not found.", sId));
-        }
-    }
-
-    public ArrayList<Supplier> get(){
-        if (sList.isEmpty()) throw new NoSuchElementException();
-        else return sList;
-    }
-    
-    public Supplier getById(int supplierId) {
-        for (Supplier supplier : sList) {
-            if (supplierId == supplier.getId()) return supplier;
-        }
-
-        throw new NoSuchElementException();
-    }
-
     public Supplier getByCNPJ(String CNPJString) {
-        for (Supplier supplier : sList) {
+        for (Supplier supplier : items) {
             if (CNPJString == supplier.getCNPJ().toString()) return supplier;
         }
 
-        throw new NoSuchElementException();
+        throw new NoSuchElementException("No supplier found with CNPJ: " + CNPJString);
     }
 }
