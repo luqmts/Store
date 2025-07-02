e9c3f2f8178c19dfaeca1340d83659dc2e223b20
lucas mateus <lucas.mts1812@gmail.com>
2025-07-02 16:54:36 -0300

    Added all dependencies for Spring work

--- src/main/java/com/luq/app/App.java
+++ src/main/java/com/luq/app/App.java
@@ -1,38 +1,11 @@
 package com.luq.app;
 
-import java.sql.Connection;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import controller.ProductController;
-import controller.SupplierController;
-
-import database.PostgresConnection;
-import database.DAO.ProductDAO;
-import database.DAO.SupplierDAO;
-
-import view.LoginView;
-import view.ProductView;
-import view.SupplierView;
-
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        Connection conn = PostgresConnection.connect();
-
-        if (conn == null) {
-            System.out.println("Error connecting to the database");
-            return;
-        }
-
-        SupplierDAO sDao = new SupplierDAO(conn);
-        ProductDAO pDao = new ProductDAO(conn);
-  
-        SupplierController sController = new SupplierController(sDao);
-        ProductController pController = new ProductController(sDao, pDao);
-
-        SupplierView sView = new SupplierView(sController);
-        ProductView pView = new ProductView(pController, sController);
-
-        LoginView lView = new LoginView(pView, sView);
-        
-        lView.showMainMenu();
+        SpringApplication.run(App.class, args);
     }
 }
