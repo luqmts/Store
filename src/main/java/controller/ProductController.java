49b3728c4e93f690d8055496f38c5df027388027
lucas mateus <lucas.mts1812@gmail.com>
2025-07-02 16:48:18 -0300

    Added Spring Annotations for controllers

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -1,27 +1,42 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import model.Product;
 import service.ProductService;
 
+@RestController
+@RequestMapping("/products")
 public class ProductController {
     private final ProductService pService;
 
+    @Autowired
     public ProductController(ProductService pService) {
         this.pService = pService;
     }
 
+    @PostMapping
     public Product registerProduct(String pSku, String pName, String pDescription, int sId){
         return pService.registerProduct(pSku, pName, pDescription, sId);
     }
 
+    @GetMapping
     public String showAllProducts(){
        return pService.showAllProducts();
     }
 
+    @PutMapping
     public Product updateProduct(int pId, String pSku, String pName, String pDescription, int sId){
         return pService.updateProduct(pId, pSku, pName, pDescription, sId);
     }
 
+    @DeleteMapping
     public int removeProduct(int pId){
         return pService.deleteProduct(pId);
     }
--- src/main/java/controller/SupplierController.java
+++ src/main/java/controller/SupplierController.java
@@ -2,27 +2,42 @@ package controller;
 
 import service.SupplierService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import model.Supplier;
 
+@RestController
+@RequestMapping("/supplier")
 public class SupplierController {
     protected final SupplierService sService;
     
+    @Autowired
     public SupplierController(SupplierService sService){
         this.sService = sService;
     }
 
+    @PostMapping
     public Supplier registerSupplier(String sName, String sCNPJ, String sMail, String sPhone){
         return sService.registerSupplier(sName, sCNPJ, sMail, sPhone);
     }
 
+    @GetMapping
     public String showAllSuppliers(){
         return sService.showAllSuppliers();
     }
 
+    @PutMapping
     public Supplier updateSupplier(int sId, String sName, String sCNPJ, String sMail, String sPhone){
         return sService.updateSupplier(sId, sName, sCNPJ, sMail, sPhone);
     }
 
+    @DeleteMapping
     public int removeSupplier(int sId){
         return sService.deleteSupplier(sId);
     }
