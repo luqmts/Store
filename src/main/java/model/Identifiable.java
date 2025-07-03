6617927d0f39d9420d8d1f587a732a7dab1e0549
lucas mateus <lucas.mts1812@gmail.com>
2025-07-03 16:49:02 -0300

    Those repositories were move to src/main/java/com/luq/storevs

--- src/main/java/model/Identifiable.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public interface Identifiable {
-    int getId();
-}
--- src/main/java/repositories/SupplierRepository.java
+++
@@ -1,8 +0,0 @@
-package repositories;
-
-import model.Supplier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SupplierRepository extends JpaRepository<Supplier, Integer>{
-    
-}
