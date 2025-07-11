1eed45808847d15f1f53e1b2efb7877ce2d75f72
lucas mateus <lucas.mts1812@gmail.com>
2025-07-10 22:50:26 -0300

    Enum Department is now in a separated file

---
+++ src/main/java/com/luq/storevs/model/Department.java
@@ -0,0 +1,26 @@
+package com.luq.storevs.model;
+
+import lombok.Getter;
+
+@Getter
+public enum Department {
+
+    TECHNOLOGY ("Technology"),
+    FOOD ("Food"),
+    CLOTHES ("Clothes");
+
+    private final String description;
+
+    Department(String description) {
+        this.description = description;
+    }
+
+    public static Department getDepartment(String description) {
+        for (Department department : Department.values()) {
+            if (department.getDescription().toUpperCase().equals(description)) {
+                return department;
+            }
+        }
+        return null;
+    }
+}
