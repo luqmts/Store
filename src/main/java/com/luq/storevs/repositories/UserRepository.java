6dc74d933088d96872edc99811d878d57b94c765
lucas mateus <lucas.mts1812@gmail.com>
2025-07-16 19:56:35 -0300

    requisites for user form and user list

--- src/main/java/com/luq/storevs/repositories/UserRepository.java
+++ src/main/java/com/luq/storevs/repositories/UserRepository.java
@@ -1,10 +1,29 @@
 package com.luq.storevs.repositories;
 
 import com.luq.storevs.domain.User.User;
+import com.luq.storevs.domain.User.UserRole;
 import com.luq.storevs.valueobjects.Mail;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, String> {
     UserDetails findByLogin(Mail login);
+
+    @Query("""
+        SELECT u FROM users u
+        WHERE (:role IS NULL OR u.role = :role)
+        AND (:name IS NULL OR u.name LIKE :name || '%')
+        AND (:login IS NULL OR u.login LIKE :login || '%')
+    """)
+    List<User> findByNameAndLoginAndRole(
+            Sort sort,
+            @Param("name") String name,
+            @Param("login") String login,
+            @Param("role") UserRole role
+    );
 }
