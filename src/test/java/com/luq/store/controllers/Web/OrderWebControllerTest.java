12355c7580f30373c1dd4e04fe9266b2b5da1c1e
lucas mateus <lucas.mts1812@gmail.com>
2025-07-22 21:18:36 -0300

    Get requisitions to list and form now validate if products, customers and sellers are correct on model

--- src/test/java/com/luq/store/controllers/Web/OrderWebControllerTest.java
+++ src/test/java/com/luq/store/controllers/Web/OrderWebControllerTest.java
@@ -62,21 +62,23 @@ public class OrderWebControllerTest {
     private Order fakeOrder1, fakeOrder2;
     private Supply fakeSupply;
     private Product fakeProduct1, fakeProduct2;
+    private Seller fakeSeller1, fakeSeller2;
+    private Customer fakeCustomer1, fakeCustomer2;
 
     @BeforeEach
     public void setUp(){
         String user = "Jimmy McGill";
         LocalDateTime now = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
 
-        Customer fakeCustomer1 = new Customer(1, "Test Customer 01", user, now, user, now);
-        Customer fakeCustomer2 = new Customer(2, "Test Customer 02", user, now, user, now);
+        fakeCustomer1 = new Customer(1, "Test Customer 01", user, now, user, now);
+        fakeCustomer2 = new Customer(2, "Test Customer 02", user, now, user, now);
 
-        Seller fakeSeller1 = new Seller(
+        fakeSeller1 = new Seller(
             1, "Walter White",
             new Mail("WalterWhite@Cooking.com"), new Phone("11901010101"), Department.FOOD,
             user, now, user, now
         );
-        Seller fakeSeller2 = new Seller(
+        fakeSeller2 = new Seller(
             2, "Jesse Pinkman",
             new Mail("Jesse Pinkman@Cooking.com"), new Phone("11904040404"), Department.FOOD,
             user, now, user, now
@@ -119,6 +121,9 @@ public class OrderWebControllerTest {
     @DisplayName("Test if all Orders are being returned on getAllSorted method with no filters applied and default user")
     public void testListAllOrders() throws Exception{
         when(oService.getAllSorted("id", "asc", null, null, null)).thenReturn(List.of(fakeOrder1, fakeOrder2));
+        when(pService.getAll()).thenReturn(List.of(fakeProduct1, fakeProduct2));
+        when(sellerService.getAll()).thenReturn(List.of(fakeSeller1, fakeSeller2));
+        when(cService.getAll()).thenReturn(List.of(fakeCustomer1, fakeCustomer2));
 
         mockMvc.perform(
             get("/order/list")
@@ -130,7 +135,10 @@ public class OrderWebControllerTest {
         .andExpect(model().attribute("orders", List.of(fakeOrder1, fakeOrder2)))
         .andExpect(model().attribute("page", "order"))
         .andExpect(model().attribute("sortBy", "id"))
-        .andExpect(model().attribute("direction", "asc"));
+        .andExpect(model().attribute("direction", "asc"))
+        .andExpect(model().attribute("products", List.of(fakeProduct1, fakeProduct2)))
+        .andExpect(model().attribute("customers", List.of(fakeCustomer1, fakeCustomer2)))
+        .andExpect(model().attribute("sellers", List.of(fakeSeller1, fakeSeller2)));
     }
 
     @Test
@@ -138,6 +146,9 @@ public class OrderWebControllerTest {
     @DisplayName("Test if Order is being returned on getAllSorted method with one filter applied and default user")
     public void testListOrdersWithOneFilter() throws Exception{
         when(oService.getAllSorted("id", "asc", 1, null, null)).thenReturn(List.of(fakeOrder1));
+        when(pService.getAll()).thenReturn(List.of(fakeProduct1, fakeProduct2));
+        when(sellerService.getAll()).thenReturn(List.of(fakeSeller1, fakeSeller2));
+        when(cService.getAll()).thenReturn(List.of(fakeCustomer1, fakeCustomer2));
 
         mockMvc.perform(
             get("/order/list")
@@ -151,7 +162,10 @@ public class OrderWebControllerTest {
         .andExpect(model().attribute("page", "order"))
         .andExpect(model().attribute("sortBy", "id"))
         .andExpect(model().attribute("direction", "asc"))
-        .andExpect(model().attribute("productId", 1));
+        .andExpect(model().attribute("productId", 1))
+        .andExpect(model().attribute("products", List.of(fakeProduct1, fakeProduct2)))
+        .andExpect(model().attribute("customers", List.of(fakeCustomer1, fakeCustomer2)))
+        .andExpect(model().attribute("sellers", List.of(fakeSeller1, fakeSeller2)));
     }
 
     @Test
@@ -159,6 +173,9 @@ public class OrderWebControllerTest {
     @DisplayName("Test if Order is being returned on getAllSorted method with all filters applied and default user")
     public void testListOrdersWithAllFilters() throws Exception{
         when(oService.getAllSorted("id", "asc", 1, 1, 1)).thenReturn(List.of(fakeOrder1));
+        when(pService.getAll()).thenReturn(List.of(fakeProduct1, fakeProduct2));
+        when(sellerService.getAll()).thenReturn(List.of(fakeSeller1, fakeSeller2));
+        when(cService.getAll()).thenReturn(List.of(fakeCustomer1, fakeCustomer2));
 
         mockMvc.perform(
             get("/order/list")
@@ -176,7 +193,10 @@ public class OrderWebControllerTest {
         .andExpect(model().attribute("direction", "asc"))
         .andExpect(model().attribute("productId", 1))
         .andExpect(model().attribute("sellerId", 1))
-        .andExpect(model().attribute("customerId", 1));
+        .andExpect(model().attribute("customerId", 1))
+        .andExpect(model().attribute("products", List.of(fakeProduct1, fakeProduct2)))
+        .andExpect(model().attribute("customers", List.of(fakeCustomer1, fakeCustomer2)))
+        .andExpect(model().attribute("sellers", List.of(fakeSeller1, fakeSeller2)));
     }
 
     @Test
@@ -184,6 +204,9 @@ public class OrderWebControllerTest {
     @DisplayName("Test if no Order is being returned on getAllSorted method correctly as a default user")
     public void testListWithNoOrders() throws Exception{
         when(oService.getAllSorted("id", "asc", 5, null, null)).thenReturn(List.of());
+        when(pService.getAll()).thenReturn(List.of(fakeProduct1, fakeProduct2));
+        when(sellerService.getAll()).thenReturn(List.of(fakeSeller1, fakeSeller2));
+        when(cService.getAll()).thenReturn(List.of(fakeCustomer1, fakeCustomer2));
 
         mockMvc.perform(
             get("/order/list")
@@ -197,29 +220,46 @@ public class OrderWebControllerTest {
         .andExpect(model().attribute("page", "order"))
         .andExpect(model().attribute("sortBy", "id"))
         .andExpect(model().attribute("direction", "asc"))
-        .andExpect(model().attribute("productId", 5));
+        .andExpect(model().attribute("productId", 5))
+        .andExpect(model().attribute("products", List.of(fakeProduct1, fakeProduct2)))
+        .andExpect(model().attribute("customers", List.of(fakeCustomer1, fakeCustomer2)))
+        .andExpect(model().attribute("sellers", List.of(fakeSeller1, fakeSeller2)));
     }
 
     @Test
     @WithMockUser
-    @DisplayName("Default user must can access Order form page (New Order)")
+    @DisplayName("Default user can access Order form page (New Order)")
     public void testOrderFormAsDefaultUser() throws Exception{
+        when(pService.getAllRegisteredOnSupply()).thenReturn(List.of(fakeProduct1, fakeProduct2));
+        when(sellerService.getAll()).thenReturn(List.of(fakeSeller1, fakeSeller2));
+        when(cService.getAll()).thenReturn(List.of(fakeCustomer1, fakeCustomer2));
+
         mockMvc.perform(get("/order/form"))
             .andExpect(status().isOk())
             .andExpect(view().name("order-form"))
             .andExpect(model().attributeExists("order"))
-            .andExpect(model().attribute("page", "order"));
+            .andExpect(model().attribute("page", "order"))
+            .andExpect(model().attribute("products", List.of(fakeProduct1, fakeProduct2)))
+            .andExpect(model().attribute("customers", List.of(fakeCustomer1, fakeCustomer2)))
+            .andExpect(model().attribute("sellers", List.of(fakeSeller1, fakeSeller2)));
     }
 
     @Test
     @WithMockUser(username = "Admin", roles = {"ADMIN"})
     @DisplayName("Admin user can access Order form page (New Order)")
     public void testOrderFormAsAdmin() throws Exception{
+        when(pService.getAllRegisteredOnSupply()).thenReturn(List.of(fakeProduct1, fakeProduct2));
+        when(sellerService.getAll()).thenReturn(List.of(fakeSeller1, fakeSeller2));
+        when(cService.getAll()).thenReturn(List.of(fakeCustomer1, fakeCustomer2));
+
         mockMvc.perform(get("/order/form"))
         .andExpect(status().isOk())
         .andExpect(view().name("order-form"))
         .andExpect(model().attributeExists("order"))
-        .andExpect(model().attribute("page", "order"));
+        .andExpect(model().attribute("page", "order"))
+        .andExpect(model().attribute("products", List.of(fakeProduct1, fakeProduct2)))
+        .andExpect(model().attribute("customers", List.of(fakeCustomer1, fakeCustomer2)))
+        .andExpect(model().attribute("sellers", List.of(fakeSeller1, fakeSeller2)));
     }
 
     @Test
@@ -228,13 +268,19 @@ public class OrderWebControllerTest {
     public void testOrderEditFormAsDefaultUser() throws Exception{
         when(oService.getById(1)).thenReturn(fakeOrder1);
         when(supplyService.getByProduct(fakeOrder1.getProduct())).thenReturn(fakeSupply);
+        when(pService.getAllRegisteredOnSupply(1)).thenReturn(List.of(fakeProduct1, fakeProduct2));
+        when(sellerService.getAll()).thenReturn(List.of(fakeSeller1, fakeSeller2));
+        when(cService.getAll()).thenReturn(List.of(fakeCustomer1, fakeCustomer2));
 
         mockMvc.perform(get("/order/form/1"))
             .andExpect(status().isOk())
             .andExpect(view().name("order-form"))
             .andExpect(model().attributeExists("order"))
             .andExpect(model().attribute("order", fakeOrder1))
-            .andExpect(model().attribute("page", "order"));
+            .andExpect(model().attribute("page", "order"))
+            .andExpect(model().attribute("products", List.of(fakeProduct1, fakeProduct2)))
+            .andExpect(model().attribute("customers", List.of(fakeCustomer1, fakeCustomer2)))
+            .andExpect(model().attribute("sellers", List.of(fakeSeller1, fakeSeller2)));
     }
 
     @Test
@@ -243,13 +289,19 @@ public class OrderWebControllerTest {
     public void testOrderEditFormAsAdmin() throws Exception{
         when(oService.getById(1)).thenReturn(fakeOrder1);
         when(supplyService.getByProduct(fakeOrder1.getProduct())).thenReturn(fakeSupply);
+        when(pService.getAllRegisteredOnSupply(1)).thenReturn(List.of(fakeProduct1, fakeProduct2));
+        when(sellerService.getAll()).thenReturn(List.of(fakeSeller1, fakeSeller2));
+        when(cService.getAll()).thenReturn(List.of(fakeCustomer1, fakeCustomer2));
 
         mockMvc.perform(get("/order/form/1"))
         .andExpect(status().isOk())
         .andExpect(view().name("order-form"))
         .andExpect(model().attributeExists("order"))
         .andExpect(model().attribute("order", fakeOrder1))
-        .andExpect(model().attribute("page", "order"));
+        .andExpect(model().attribute("page", "order"))
+        .andExpect(model().attribute("products", List.of(fakeProduct1, fakeProduct2)))
+        .andExpect(model().attribute("customers", List.of(fakeCustomer1, fakeCustomer2)))
+        .andExpect(model().attribute("sellers", List.of(fakeSeller1, fakeSeller2)));
     }
 
     @Test
