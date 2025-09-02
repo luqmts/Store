8045abafb655def7d503a541baeb695f02cc5dea
lucas mateus <lucas.mts1812@gmail.com>
2025-09-02 00:00:41 -0300

    Added status filter on repository file for Order

--- src/main/java/com/luq/store/repositories/OrderRepository.java
+++ src/main/java/com/luq/store/repositories/OrderRepository.java
@@ -4,6 +4,7 @@ import com.luq.store.domain.Order;
 
 import java.util.List;
 
+import com.luq.store.domain.OrderStatus;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,12 +13,14 @@ import org.springframework.data.repository.query.Param;
 public interface OrderRepository extends JpaRepository<Order, Integer>{
     @Query("""
         SELECT o FROM Order o
-        WHERE (:productId IS NULL OR o.product.id = :productId)
+        WHERE (:status IS NULL OR o.status = :status)
+        AND (:productId IS NULL OR o.product.id = :productId)
         AND (:sellerId IS NULL OR o.seller.id = :sellerId)
         AND (:customerId IS NULL OR o.customer.id = :customerId)
     """)
-    List<Order> findByProductSellerCustomer(
+    List<Order> findByStatusProductSellerCustomer(
         Sort sort,
+        @Param("status") OrderStatus status,
         @Param("productId")Integer productId, 
         @Param("sellerId")Integer sellerId, 
         @Param("customerId")Integer customerId
