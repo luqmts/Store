4937caf2049bbe0e8f51b874a5f82b496fc6ca51
lucas mateus <lucas.mts1812@gmail.com>
2025-09-07 18:03:33 -0300

    Added properties on RestControllerAdvice anottation to just api errors be handled

--- src/main/java/com/luq/store/exceptions/ErrorHandling.java
+++ src/main/java/com/luq/store/exceptions/ErrorHandling.java
@@ -3,12 +3,13 @@ package com.luq.store.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@RestControllerAdvice
+@RestControllerAdvice(annotations = RestController.class)
 public class ErrorHandling {
     Map<String, String> errors = new HashMap<>();
 
